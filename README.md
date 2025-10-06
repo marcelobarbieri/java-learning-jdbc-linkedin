793f776b437e77aa3ca21025fef0c86adf5228bf
Marcelo de Paiva Barbieri <86416342+marcelobarbieri@users.noreply.github.com>
2025-10-06 14:09:12 +0000

    Partial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # java-learning-jdbc-linkedin
+
+```
+$ chmod +x dat/load-db.sh
+$ dat/load-db.sh
+$ psql -U $POSTGRES_USER -d $POSTGRES_DB -h localhost
+# select * from wisdom.services;
+```
+
+```
+mvn archetype:generate -DgroupId=com.frankmoley.lil -DartifactId=learning-jdbc -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5 -DinteractiveMode=false
+
+mvn validate
+mvn clean install
+```
---
+++ learning-jdbc/src/main/java/com/frankmoley/lil/data/util/DatabaseUtils.java
@@ -0,0 +1,46 @@
+package com.frankmoley.lil.data.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+
+public class DatabaseUtils {
+
+    private static final String URL = "jdbc:postresql://localhost:5432/localdb";
+    private static final String USERNAME = "localdbuser";
+    private static final String PASSWORD = "P@ssw0rd!";
+    private static final Logger LOGGER = Logger.getLogger(DatabaseUtils.class.getName());
+
+    private static final String exceptionFormat = "exception in %s, message: %s, code: %s";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            synchronized(DatabaseUtils.class) {
+                if (connection == null) {
+                    try {
+                        connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+                    } catch (SQLException e) {
+                        handleSQLException("DatabaseUtils.getConnection",e,LOGGER);
+                    }
+                }
+            }
+        }
+        return connection;
+    }
+
+    public static void handleSQLException (String method, SQLException e, Logger log) {
+        log.warning (
+            String.format(
+                exceptionFormat,
+                method,
+                e.getMessage(),
+                e.getErrorCode()
+            )
+        );
+
+        throw new RuntimeException(e);
+    }
+}
