ae88b2c0586e663965ae0d1abac76155f517ddef
Marcelo de Paiva Barbieri <86416342+marcelobarbieri@users.noreply.github.com>
2025-10-06 18:33:38 +0000

    Customers CRUD

--- learning-jdbc/src/main/java/com/frankmoley/lil/App.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/App.java
@@ -4,17 +4,21 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+import com.frankmoley.lil.data.dao.CustomerDao;
 import com.frankmoley.lil.data.dao.ServiceDao;
+import com.frankmoley.lil.data.entity.Customer;
 import com.frankmoley.lil.data.entity.Service;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        ServiceDao serviceDao = new ServiceDao();
-        
+                
+        System.out.println("**************************************************");
         System.out.println("*** SERVICES ***");
+        System.out.println("**************************************************");
+
+        ServiceDao serviceDao = new ServiceDao();
+
         System.out.println("*** GET ALL");
         List<Service> services = serviceDao.getAll();
         services.forEach(System.out::println);
@@ -42,5 +46,40 @@ public class App {
         System.out.println("*** LIST");
         services.forEach(System.out::println);        
 
+        System.out.println("**************************************************");
+        System.out.println("*** CUSTOMERS ***");
+        System.out.println("**************************************************");
+
+        CustomerDao customerDao = new CustomerDao();
+
+        System.out.println("*** GET ALL");
+        List<Customer> customers = customerDao.getAll();
+        customers.forEach(System.out::println);
+
+        System.out.println("*** GET ONE");
+        Optional<Customer> customer = customerDao.getOne(customers.get(0).getCustomerId());
+        System.out.println(customer.get());
+
+        System.out.println("*** CREATE");
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstName("FirstName" + System.currentTimeMillis());
+        newCustomer.setLastName("LastName" + System.currentTimeMillis());
+        newCustomer.setEmail("Email" + System.currentTimeMillis());
+        newCustomer.setPhone("Phone" + System.currentTimeMillis());
+        newCustomer.setAddress("Address" + System.currentTimeMillis());
+        newCustomer = customerDao.create(newCustomer);
+        System.out.println(newCustomer);
+
+        System.out.println("*** UPDATE");
+        newCustomer.setEmail("Email Updated "  + System.currentTimeMillis());
+        newCustomer = customerDao.update(newCustomer);
+        System.out.println(newCustomer);
+
+        System.out.println("*** DELETE");
+        System.out.println(newCustomer);
+        serviceDao.delete(newCustomer.getCustomerId());        
+
+        System.out.println("*** LIST");
+        customers.forEach(System.out::println);          
     }
 }
---
+++ learning-jdbc/src/main/java/com/frankmoley/lil/data/dao/CustomerDao.java
@@ -0,0 +1,151 @@
+package com.frankmoley.lil.data.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import com.frankmoley.lil.data.entity.Customer;
+import com.frankmoley.lil.data.util.DatabaseUtils;
+
+public class CustomerDao  implements Dao<Customer,UUID> {
+
+    private static final Logger LOGGER = Logger.getLogger(CustomerDao.class.getName());
+    
+    private static final String GET_ALL = "select customer_id, first_name, last_name, email, phone, address from wisdom.customers";
+    private static final String GET_BY_ID = "select customer_id, first_name, last_name, email, phone, address from wisdom.customers where customer_id = ?";
+    private static final String CREATE = "insert into wisdom.customers (customer_id, first_name, last_name, email, phone, address) values(?,?,?,?,?,?)";
+    private static final String UPDATE = "update wisdom.customers set first_name = ?, last_name = ?, email = ?, phone = ?, address = ? where customer_id = ?";
+    private static final String DELETE = "delete from wisdom.customers where customer_id = ?";
+
+    @Override
+    public Customer create(Customer entity) {
+        UUID customerId = UUID.randomUUID();
+        Connection connection = DatabaseUtils.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(CREATE);            
+            statement.setObject(1,customerId);
+            statement.setObject(2, entity.getFirstName());
+            statement.setObject(3, entity.getLastName());
+            statement.setObject(4, entity.getEmail());
+            statement.setObject(5, entity.getPhone());
+            statement.setObject(6, entity.getAddress());            
+            
+            statement.execute();
+            connection.commit();
+            statement.close();
+        } catch (SQLException e1) {
+            try {
+                connection.rollback();
+            } catch (SQLException e2) {
+                DatabaseUtils.handleSQLException("CustomerDao.create.rollback", e2, LOGGER);
+            }
+            DatabaseUtils.handleSQLException("CustomerDao.create", e1, LOGGER);
+        }
+        Optional<Customer> customer = this.getOne(customerId);
+        if (!customer.isPresent())
+            return null;
+        return customer.get();
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Connection connection = DatabaseUtils.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(DELETE);
+            statement.setObject(1, id);
+            statement.executeUpdate();
+            connection.commit();
+            statement.close();
+        } catch (SQLException e1) {
+            try {
+                connection.rollback();
+            } catch (SQLException e2) {
+                DatabaseUtils.handleSQLException("CustomerDao.delete.rollback", e2, LOGGER);
+            }
+            DatabaseUtils.handleSQLException("CustomerDao.delete", e1, LOGGER);
+        }          
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        List<Customer> customers = new ArrayList<>();
+        Connection connection = DatabaseUtils.getConnection();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(GET_ALL);
+            customers = this.processResultSet(rs);
+        } catch (SQLException e) {
+            DatabaseUtils.handleSQLException("CustomerDao.getAll", e, LOGGER);
+        }
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> getOne(UUID id) {
+        try (PreparedStatement statement = DatabaseUtils.getConnection().prepareStatement(GET_BY_ID)) {
+            statement.setObject(1,id); // non zero-based, replace ? from GET_BY_ID
+            ResultSet rs = statement.executeQuery();
+            List<Customer> customers = this.processResultSet(rs);
+            if (customers.isEmpty()) 
+                return Optional.empty();
+            return Optional.of(customers.get(0));
+        } catch (SQLException e) {
+            DatabaseUtils.handleSQLException("CostumerDao.getOne", e, LOGGER);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Customer update(Customer entity) {
+        Connection connection = DatabaseUtils.getConnection();
+        
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(UPDATE);
+            statement.setString(1, entity.getFirstName());
+            statement.setString(2, entity.getLastName());
+            statement.setString(3, entity.getEmail());
+            statement.setString(4, entity.getPhone());
+            statement.setString(5, entity.getAddress());
+            statement.setObject(6, entity.getCustomerId());
+            
+            statement.execute();
+            connection.commit();
+            statement.close();
+        } catch (SQLException e1) {
+            try {
+                connection.rollback();
+            } catch (SQLException e2) {
+                DatabaseUtils.handleSQLException("CustomerDao.update.rollback", e2, LOGGER);
+            }
+            DatabaseUtils.handleSQLException("CustomerDao.update", e1, LOGGER);
+        }
+        return this.getOne(entity.getCustomerId()).get();
+    }
+
+    private List<Customer> processResultSet(ResultSet rs) throws SQLException {                
+
+        List<Customer> customers = new ArrayList<>();
+
+        while(rs.next()) {
+            Customer costumer = new Customer();
+            costumer.setCustomerId((UUID)rs.getObject("customer_id"));
+            costumer.setFirstName(rs.getString("first_name"));
+            costumer.setLastName(rs.getString("last_name"));
+            costumer.setEmail(rs.getString("email"));
+            costumer.setPhone(rs.getString("phone"));
+            costumer.setAddress(rs.getString("address"));
+            customers.add(costumer);
+        }
+        return customers;
+    }       
+
+}
